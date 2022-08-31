90b4ed3bb2cf92a749c83ed415a50f46b33bc8bb
Ste-Viera <stephanie_holzschuk@hotmail.com>
2022-08-30 22:03:02 -0300

    Aula Rotas

---
+++ angular02/src/app/diretiva-for/diretiva-for.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-diretiva-for',
+  templateUrl: './diretiva-for.component.html',
+  styleUrls: ['./diretiva-for.component.css']
+})
+export class DiretivaForComponent implements OnInit {
+
+  alunos: Array<string> = new Array()
+  nome!:string
+  nomes!:Array<string>
+
+  constructor() { }
+
+  ngOnInit(): void {
+    
+  }
+
+  deletar(index: number) {
+    this.alunos.splice(index, 1)
+  }
+
+  adicionar(){
+    if(this.nome){
+      this.alunos.push(this.nome)
+      this.nome=""
+    }
+  }
+}
